ee69257bdc42b0d37ab15a21393130ec23ff81a3
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2021-11-28 20:28:32 +0300

    stash changes

--- contracts/batch-sender.sol
+++ contracts/batch-sender.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.6;
 
-contract BatchSender {
-    address payable private owner;
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract BatchSender is PullPayment {
+    using SafeMath for uint256;
+    using EnumerableMap for EnumerableMap.UintToAddressMap;
+
+    address payable private _owner;
 
     event EthTransferred(address receiver, uint amount);
     event Deposit(uint value);
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not owner");
+        require(msg.sender == _owner, "Not owner");
         _;
     }
 
     constructor () payable {
-        owner = payable(msg.sender);
+        _owner = payable(msg.sender);
     }
 
     fallback() external payable {
@@ -24,22 +30,27 @@ contract BatchSender {
         emit Deposit(msg.value);
     }
 
-    function setOwner(address payable newOwner) public onlyOwner{
-        require(newOwner != owner, "Same owner");
-
-        owner = newOwner;
-    }
-
     function sendEther(
         address receiver1, uint amount1,
-        address receiver2, uint amount2
-    ) public onlyOwner {
-        (bool success, ) = receiver1.call{value: amount1}("");
-        require(success, "Failed to send Ether");
-        emit EthTransferred(receiver1, amount1);
+        address receiver2, uint amount2,
+        EnumerableMap.UintToAddressMap balanceMap
+    ) public payable nonReentrant onlyOwner {
+        require(msg.sender.isContract == false, "Contract receivers are not supported");
 
-        (success, ) = receiver2.call{value: amount2}("");
-        require(success, "Failed to send Ether");
-        emit EthTransferred(receiver2, amount2);
+        // Sum the amounts to pay
+        // for (uint256 i = 0; i < balanceMap.length; i++) {
+        //     totalAmountToTransfer += EnumerableMap.UintToAddressMap.at(balanceMap, i);
+        // }
+        totalAmountToTransfer = amount1 + amount2; // TODO: remove 
+
+        // Send to all recipients
+        {
+            // TODO: Check balance 
+            // require(balances[msg.sender] >= totalAmountToTransfer, "Not enough balance");
+
+            (bool success, ) = receiver1.call{value: amount1}("");
+            require(success, "Failed to send Ether");
+            emit EthTransferred(receiver1, amount1);
+        }
     }
 }
